a49672408fe6982f658b4168658cfdacebd8015e
Ranus Ate <radianus.ra@mbteknologi.com>
2021-09-19 05:59:10 +0700

    update

--- omdb/script.js
+++ omdb/script.js
@@ -1,31 +1,100 @@
-$(".input-keyword").on("keyup", function () {
-  $.ajax({
-    url:
-      "http://www.omdbapi.com/?apikey=3192400b&i=tt3896198&s=" +
-      $(".input-keyword").val(),
-    success: function (response) {
-      let movie = response.Search;
+// $(".input-keyword").on("keyup", function () {
+//   $.ajax({
+//     url:
+//       "http://www.omdbapi.com/?apikey=3192400b&i=tt3896198&s=" +
+//       $(".input-keyword").val(),
+//     success: function (response) {
+//       let movie = response.Search;
+//       let cards = "";
+//       movie.forEach(function (m) {
+//         cards += showCards(m);
+//       });
+//       $(".movie-container").html(cards);
+//       $(".modal-btn-detail").on("click", function () {
+//         $.ajax({
+//           url:
+//             "http://www.omdbapi.com/?apikey=3192400b&i=" +
+//             $(this).data("imdbid"),
+//           success: function (data) {
+//             const movieDetail = `
+//               <div class="container-fluid">
+//               <div class="row">
+//               <div class="col-md-3">
+//               <img src="${data.Poster}" alt="${data.Title}"  class="img-fluid">
+//               </div>
+//               <div class="col-md">
+//               <ul class="list-group">
+//               <li class="list-group-item">
+//               <H4>${data.Title} (${data.Year})</H4>
+//               </li>
+//               <li class="list-group-item"><strong>Director: </strong> ${data.Director} </li>
+//               <li class="list-group-item"><strong>Actors: </strong> ${data.Actors} </li>
+//               <li class="list-group-item"><strong>Write : </strong>${data.Writer}</li>
+//               <li class="list-group-item"><strong>Plot : </strong><br> ${data.Plot}</li>
+//               </ul>
+//               </div>
+//               </div>
+//               </div>`;
+//             $(".modal-body").html(movieDetail);
+//           },
+//           error: function (e) {
+//             alert(e.responseText);
+//           },
+//         });
+//       });
+//     },
+//     error: function (e) {
+//       alert(e.responseText);
+//     },
+//   });
+// });
+
+const seacBtn = document.querySelector(".input-keyword");
+seacBtn.addEventListener("keyup", function () {
+  const keyword = document.querySelector(".input-keyword");
+  fetch(
+    "http://www.omdbapi.com/?apikey=3192400b&i=tt3896198&s=" + keyword.value
+  )
+    .then((response) => response.json())
+    .then((response) => {
       let cards = "";
-      movie.forEach(function (m) {
-        cards += `<div class="col-md-4 my-2">
-        <div class="card">
-        <img src="${m.Poster}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${m.Title}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                <a href="#" class="btn btn-primary modal-btn-detail" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid = ${m.imdbID} >Detail</a>
-            </div>
-        </div>
-    </div>`;
+      response.Search.forEach((m) => {
+        cards += showCards(m);
       });
-      $(".movie-container").html(cards);
-      $(".modal-btn-detail").on("click", function () {
-        $.ajax({
-          url:
-            "http://www.omdbapi.com/?apikey=3192400b&i=" +
-            $(this).data("imdbid"),
-          success: function (data) {
-            const movieDetail = `
+      let content = document.querySelector(".movie-container");
+      content.innerHTML = cards;
+    })
+    .then(() => {
+      const btnDetail = document.querySelectorAll(".modal-btn-detail");
+      btnDetail.forEach((btn) => {
+        btn.addEventListener("click", function () {
+          const idMovie = this.dataset.imdbid;
+          fetch("http://www.omdbapi.com/?apikey=3192400b&i=" + idMovie)
+            .then((response) => response.json())
+            .then((response) => {
+              const modalBody = document.querySelector(".modal-body");
+              modalBody.innerHTML = showDetailModal(response);
+            });
+        });
+      });
+    });
+});
+
+function showCards(m) {
+  return `<div class="col-md-4 my-2">
+          <div class="card">
+          <img src="${m.Poster}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">${m.Title}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                  <a href="#" class="btn btn-primary modal-btn-detail" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid = ${m.imdbID} >Detail</a>
+              </div>
+          </div>
+      </div>`;
+}
+
+function showDetailModal(data) {
+  return `
               <div class="container-fluid">
               <div class="row">
               <div class="col-md-3">
@@ -44,75 +113,4 @@ $(".input-keyword").on("keyup", function () {
               </div>
               </div>
               </div>`;
-            $(".modal-body").html(movieDetail);
-          },
-          error: function (e) {
-            alert(e.responseText);
-          },
-        });
-      });
-    },
-    error: function (e) {
-      alert(e.responseText);
-    },
-  });
-});
-$(".search-btn").on("click", function () {
-  $.ajax({
-    url:
-      "http://www.omdbapi.com/?apikey=3192400b&i=tt3896198&s=" +
-      $(".input-keyword").val(),
-    success: function (response) {
-      let movie = response.Search;
-      let cards = "";
-      movie.forEach(function (m) {
-        cards += `<div class="col-md-4 my-2">
-          <div class="card">
-          <img src="${m.Poster}" class="card-img-top" alt="...">
-              <div class="card-body">
-                  <h5 class="card-title">${m.Title}</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                  <a href="#" class="btn btn-primary modal-btn-detail" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid = ${m.imdbID} >Detail</a>
-              </div>
-          </div>
-      </div>`;
-      });
-      $(".movie-container").html(cards);
-      $(".modal-btn-detail").on("click", function () {
-        $.ajax({
-          url:
-            "http://www.omdbapi.com/?apikey=3192400b&i=" +
-            $(this).data("imdbid"),
-          success: function (data) {
-            const movieDetail = `
-                <div class="container-fluid">
-                <div class="row">
-                <div class="col-md-3">
-                <img src="${data.Poster}" alt="${data.Title}"  class="img-fluid">
-                </div>
-                <div class="col-md">
-                <ul class="list-group">
-                <li class="list-group-item">
-                <H4>${data.Title} (${data.Year})</H4>
-                </li>
-                <li class="list-group-item"><strong>Director: </strong> ${data.Director} </li>
-                <li class="list-group-item"><strong>Actors: </strong> ${data.Actors} </li>
-                <li class="list-group-item"><strong>Write : </strong>${data.Writer}</li>
-                <li class="list-group-item"><strong>Plot : </strong><br> ${data.Plot}</li>
-                </ul>
-                </div>
-                </div>
-                </div>`;
-            $(".modal-body").html(movieDetail);
-          },
-          error: function (e) {
-            alert(e.responseText);
-          },
-        });
-      });
-    },
-    error: function (e) {
-      alert(e.responseText);
-    },
-  });
-});
+}
