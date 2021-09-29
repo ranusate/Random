ad1b3226b6202c04202177429a91692ee9c275a6
Ranus Ate <radianus.ra@mbteknologi.com>
2021-09-29 15:29:10 +0700

    update

--- Javas/Test/src/App.java
+++ Javas/Test/src/App.java
@@ -1,26 +1,27 @@
 import java.util.Scanner;
+
 public class App {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
         int bil, awal, akhir;
-        
-         System.out.println("PROGRAM JAVA MENAMPILKAN DERET BILANGAN PRIMA");
-         System.out.print("Mulai dari : ");
-         awal=input.nextInt();
-         System.out.print("Sampai : ");
-         akhir=input.nextInt();
-         System.out.println("----------------------------------------------");
-        for (int i=awal; i<=akhir; i++){
-            bil=0;
-            for (int j=1;j<=i;j++){
-                if (i%j==0){
-                    bil=bil+1;
+
+        System.out.println("PROGRAM JAVA MENAMPILKAN DERET BILANGAN PRIMA");
+        System.out.print("Mulai dari : ");
+        awal = input.nextInt();
+        System.out.print("Sampai : ");
+        akhir = input.nextInt();
+        System.out.println("----------------------------------------------");
+        input.close();
+        for (int i = awal; i <= akhir; i++) {
+            bil = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    bil = bil + 1;
                 }
             }
-            if (bil==2){
-                System.out.print(i+" ");
-            }             
-        }    
-   }
+            if (bil == 2) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
